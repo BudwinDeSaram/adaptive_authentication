03d5876262553def28cef98c5042c0302a15342b
BudwinDeSaram <dinura.20210387@iit.ac.lk>
2025-03-16 18:54:16 +0530

    change subprocess command

--- authenticator/flexfringe.py
+++ authenticator/flexfringe.py
@@ -80,10 +80,9 @@ def updatelsm(data, useremail):
     MODEL_DAT_PATH = os.path.join(MODEL_FOLDER, f"{username}_model.dat")
 
     if not os.path.exists(MODEL_DAT_PATH):
-        command = "touch " + MODEL_DAT_PATH
-        subprocess.run([command], check=True)
-
-        os.chmod(MODEL_DAT_PATH, 0o777)         
+        os.makedirs(os.path.dirname(MODEL_DAT_PATH), exist_ok=True)
+        subprocess.run(['touch', MODEL_DAT_PATH], check=True)
+        os.chmod(MODEL_DAT_PATH, 0o777)       
 
         with open(MODEL_DAT_PATH, "w") as dat_file:
             dat_file.write("1 100" + "\n") 

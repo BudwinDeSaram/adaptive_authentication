4d3441194d8c81d170b5415465d15f0bf4498ef8
BudwinDeSaram <dinura.20210387@iit.ac.lk>
2025-03-16 16:16:24 +0530

    Change file permission

--- authenticator/flexfringe.py
+++ authenticator/flexfringe.py
@@ -80,12 +80,9 @@ def updatelsm(data, useremail):
     MODEL_DAT_PATH = os.path.join(MODEL_FOLDER, f"{username}_model.dat")
 
     if not os.path.exists(MODEL_DAT_PATH):
-        command = "chmod -R 777 " + MODEL_DAT_PATH
-        subprocess.run(
-            [command],
-            check = True
-        )
-
+        os.makedirs(os.path.dirname(MODEL_DAT_PATH), exist_ok=True)  
+        os.chmod(MODEL_DAT_PATH, 0o777)
+        
         with open(MODEL_DAT_PATH, "w") as dat_file:
             dat_file.write("1 100" + "\n") 
 
